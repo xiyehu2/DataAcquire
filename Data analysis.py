f7bcb2d60ea9a28251127a4f679f0bb1fb2a1cf4
yaox2 <44078012+yaox2@users.noreply.github.com>
2018-10-19 13:44:50 -0500

    Update Data analysis.py

--- Data analysis.py
+++ Data analysis.py
@@ -4,14 +4,14 @@ Created on Thu Oct  4 08:09:20 2018
 
 @author: 14625
 """
-
+import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 
-bme_data=np.genfromtxt("D:\\Physics398DLP\\bme_and_time_data.txt", delimiter = ',', skip_header = 3, skip_footer = 1)
+data=np.genfromtxt("D:\\Physics398DLP\\8_hour_test_run.txt", delimiter = ',', skip_header = 4, skip_footer = 1)
 
 
-count = int(len(bme_data)/2)
+count = int(len(data)/2)
 
 time_hour = np.zeros(count)
 time_min = np.zeros(count)
@@ -23,23 +23,27 @@ humidity = np.zeros(count)
 altitude = np.zeros(count)
 seconds = np.zeros(count)
 
-#print(bme_data)
+#print(data)
 
 
 
-for n in range(0,len(bme_data)):
-    if n%2 ==0:
-        time_hour[int(n/2)] = (bme_data[n,0])
-        time_min[int(n/2)] = (bme_data[n,1])
-        time_sec[int(n/2)] = (bme_data[n,2])
-        time_ms[int(n/2)] = (bme_data[n,3])
+for n in range(0,len(data)):
+    if n%2 == 0:
+        time_hour[int(n/2)] = (data[n,0])
+        time_min[int(n/2)] = (data[n,1])
+        time_sec[int(n/2)] = (data[n,2])
+        time_ms[int(n/2)] = (data[n,3])
     else:
-        temperature[int((n-1)/2)] = (bme_data[n,0])
-        pressure[int((n-1)/2)] = (bme_data[n,1])
-        humidity[int((n-1)/2)] = (bme_data[n,2])
-        altitude[int((n-1)/2)] = (bme_data[n,3])
+        temperature[int((n-1)/2)] = (data[n,0])
+        pressure[int((n-1)/2)] = (data[n,1])
+        humidity[int((n-1)/2)] = (data[n,2])
+        altitude[int((n-1)/2)] = (data[n,3])
         
-seconds = time_hour*3600 + time_min*60 + time_sec + time_ms*0.001
+
+t0 = time_hour[0]*3600 + time_min[0]*60 + time_sec[0] + time_ms[0]*0.001
+init_time = np.linspace(t0,t0,count)
+seconds = time_hour*3600 + time_min*60 + time_sec + time_ms*0.001 - init_time
+
         
 
 plt.plot(seconds,temperature, 'y')
@@ -60,8 +64,23 @@ plt.xlabel('t')
 plt.ylabel('H')
 plt.show()
 
-plt.plot(seconds,altitude, 'k')
-plt.title('Altitude(m) vs Time')
-plt.xlabel('t')
-plt.ylabel('m')
-plt.show()
+#plt.plot(seconds,altitude, 'k')
+#plt.title('Altitude(m) vs Time')
+#plt.xlabel('t')
+#plt.ylabel('m')
+#plt.show()
+
+
+
+
+
+# syncronize clocks different devices
+# measure settling time for humidity, etc.
+# anamometer
+# pcb and bredboard ready
+# case design
+
+# check the heater on bme
+# test IR sensor
+# still anamometer
+# still settling time
