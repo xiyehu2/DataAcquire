5cea60fee318834f15e27dd6a23358c24bdd2716
yaox2 <44078012+yaox2@users.noreply.github.com>
2018-11-29 06:46:44 -0600

    Update Data analysis.py

--- Data analysis.py
+++ Data analysis.py
@@ -6,12 +6,15 @@ Created on Thu Oct  4 08:09:20 2018
 """
 import datetime
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
-data=np.genfromtxt("D:\\Physics398DLP\\8_hour_test_run.txt", delimiter = ',', skip_header = 4, skip_footer = 1)
-
-
-count = int(len(data)/2)
+#data = pd.read_csv("D:\\Physics398DLP\\Test_10_17_2018_1.txt", delimiter = ',', skiprows = 3)
+#data = np.genfromtxt("D:\\Physics398DLP\\Test_10_17_2018_1.txt", delimiter = ',', skip_header = 4, skip_footer = 1)
+#data_2 = np.genfromtxt("C:\\Users\\14625\\OneDrive\\Documents\\GitHub\\DataAcquire\\test_11_7_inside_doorclose_1.txt", delimiter = ',', skip_header = 4, skip_footer = 1, missing_values = 0)
+data = pd.read_csv("C:\\Users\\14625\\OneDrive\\Documents\\GitHub\\DataAcquire\\test_11_7_inside_doorclose_1.txt", delimiter = ',', skiprows = 2, skipfooter = 1,engine = 'python')
+data = data.values
+count = int(len(data)/3)
 
 time_hour = np.zeros(count)
 time_min = np.zeros(count)
@@ -22,22 +25,25 @@ pressure = np.zeros(count)
 humidity = np.zeros(count)
 altitude = np.zeros(count)
 seconds = np.zeros(count)
+anemometer = np.zeros(count)
 
 #print(data)
 
 
 
 for n in range(0,len(data)):
-    if n%2 == 0:
-        time_hour[int(n/2)] = (data[n,0])
-        time_min[int(n/2)] = (data[n,1])
-        time_sec[int(n/2)] = (data[n,2])
-        time_ms[int(n/2)] = (data[n,3])
+    if n%3 == 0:
+        time_hour[int(n/3)] = (data[n,0])
+        time_min[int(n/3)] = (data[n,1])
+        time_sec[int(n/3)] = (data[n,2])
+        time_ms[int(n/3)] = (data[n,3])
+    elif n%3 == 1:
+        temperature[int((n-1)/3)] = (data[n,0])
+        pressure[int((n-1)/3)] = (data[n,1])
+        humidity[int((n-1)/3)] = (data[n,2])
+        altitude[int((n-1)/3)] = (data[n,3])
     else:
-        temperature[int((n-1)/2)] = (data[n,0])
-        pressure[int((n-1)/2)] = (data[n,1])
-        humidity[int((n-1)/2)] = (data[n,2])
-        altitude[int((n-1)/2)] = (data[n,3])
+        anemometer[int((n-2)/3)] = data[n,0]
         
 
 t0 = time_hour[0]*3600 + time_min[0]*60 + time_sec[0] + time_ms[0]*0.001
